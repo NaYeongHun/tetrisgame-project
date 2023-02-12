431ca6abf900b21390801a6f94fb998118379be4
NaYeongHun <dudgns6928@naver.com>
2023-02-13 00:52:40 +0900

    seven  commit

--- Downloads/Telegram Desktop/tetris.py
+++ Downloads/Telegram Desktop/tetris.py
@@ -1,5 +1,6 @@
 import pygame
 from copy import deepcopy
+from random import choice, randrange
 
 WEIGHT, HEIGHT = 10, 20
 TILE = 45
@@ -26,21 +27,28 @@ figures_pos = [[(-1, 0), (-2, 0), (0, 0), (1, 0)],
 # 2차원배열로 생성
 blocks = [[pygame.Rect(x + WEIGHT// 2, y+1 , 1, 1) for x, y in fig_pos] for fig_pos in figures_pos]
 block_rect = pygame.Rect(0, 0, TILE-2 , TILE-2 )
+field = [[0 for i in range(WEIGHT)] for j in range(HEIGHT)]
 
 # 속도와 y축 대한 변수
 count, speed, limit = 0, 60, 2000
 
-block = deepcopy(blocks[3])
+block = deepcopy(choice(blocks))
 
 #블록이 벽을 벗어나지 않게 해주는 코드
 def check_borders():
     if block[i].x < 0 or block[i].x > WEIGHT - 1:
+        return False
+
+    # 맨 밑에 막아주기(쌓기)    
+    elif block[i].y > HEIGHT - 1 or field[block[i].y][block[i].x]:
         return False
     return True
 
 while True:
     # x축 이동
     dx = 0
+    # 블록 회전
+    rotate = False
     SURPACE.fill(pygame.Color('black'))
 
     for event in pygame.event.get():
@@ -55,6 +63,9 @@ while True:
             # 다운 키를 눌렀을때 빨리 내려오기
             elif event.key == pygame.K_DOWN:
                 limit = 100
+            # 블록 회전
+            elif event.key == pygame.K_UP:
+                rotate =True
 
     # 격자(화면) 그리기
     [pygame.draw.rect(SURPACE,(40,40,40), i_rect,1) for i_rect in grid]
@@ -74,15 +85,38 @@ while True:
         for i in range(4):
             block[i].y += 1
             if not check_borders():
-                    block =deepcopy(block_old)
-                    limit =100
-                    break
+                for i in range(4):
+                    field[block_old[i].y][block_old[i].x] = pygame.Color('white')
+                block =deepcopy(choice(blocks))
+                # 이 숫자가 커야 다음 블록도 빨리 안떨어짐
+                limit =2000
+                break
 
+    # 블록 회전하기
+    # 가운데를 기준으로 돌리기
+    center = block[0]
+    block_old = deepcopy(block)
+    if rotate :
+        for i in range(4):
+            x = block[i].y - center.y
+            y = block[i].x - center.x
+            block[i].x = center.x - x
+            block[i].y = center.y + y
+            if not check_borders():
+                block =deepcopy(block_old)
+                break
     # 하나 블록 그리기
     for i in range(4):
         block_rect.x =block[i].x * TILE
         block_rect.y =block[i].y * TILE
         pygame.draw.rect(SURPACE, pygame.Color('white'),block_rect)
+
+    # 필드 그리기
+    for y, raw in enumerate(field):
+        for x, col in enumerate(raw):
+            if col:
+                block_rect.x, block_rect.y = x * TILE, y * TILE
+                pygame.draw.rect(SURPACE, col, block_rect)
     
 
     pygame.display.flip()
